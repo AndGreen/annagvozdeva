03a5a894030aaf025acfd55330c0d9504a931578
Andrew <zelenin.andrew@gmail.com>
2022-02-11 00:29:15 +0300

    art groups

---
+++ common/data.js
@@ -0,0 +1,11 @@
+export const artGroups = [
+  {
+    name: 'graphic', title: 'Graphic art', arts: [
+      { link: 'graphic', src: '/2.png', width: 2221, height: 2686 },
+      { link: 'graphic', src: '/3.png', width: 3508, height: 2408 },
+    ],
+  },
+  { name: 'digital', title: 'Digital', arts: [{ link: 'graphic', src: '/1.png', width: 2480, height: 3508 }] },
+  { name: 'vector', title: 'Vector', arts: [{ link: 'graphic', src: '/v1.png', width: 4000, height: 5607 }] },
+  { name: 'painting', title: 'Painting', arts: [{ link: 'graphic', src: '/p1.png', width: 3024, height: 4032 }] },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,57 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import {Link, Element} from 'react-scroll';
+import { artGroups } from '../common/data';
+import { Link, Element } from 'react-scroll';
 
-const Item = ({link, src, width, height, layout}) =>
-  <Element name={link}>
-    <Image layout={layout} src={src} width={width} height={height} alt="image"/>
-  </Element>
+const ArtGroup = ({ name, children }) => <Element name={name}>{children}</Element>;
 
-const NavItem = ({children, to}) =>
-  <Link activeClass="font-semibold" to={to} spy={true} smooth={true} offset={50}
+const Art = ({ src, width, height, layout }) =>
+    <Image layout={layout} src={src} width={width} height={height} alt='image' />
+
+const NavItem = ({ children, to }) =>
+  <Link activeClass='font-semibold' to={to} spy={true} smooth={true} offset={50}
         duration={500}>
-    <li className="list-none mt-2 text-sm cursor-pointer">{children}</li>
-  </Link>
+    <li className='list-none mt-2 text-sm cursor-pointer'>{children}</li>
+  </Link>;
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Anna Gvozdeva</title>
-        <meta name="description" content="Illustrator️"/>
-        <link rel="icon" href="/favicon.ico"/>
+        <meta name='description' content='Illustrator️' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="flex flex-1 flex-col md:flex-row min-h-screen">
-        <sidemenu className="bg-white z-10 p-10 md:pt-16 flex flex-row md:flex-col justify-between min-w-full md:min-w-0 md:min-w-auto md:min-h-screen fixed ">
+      <main className='flex flex-1 flex-col md:flex-row min-h-screen'>
+        <sidemenu
+          className='bg-white z-10 p-10 md:pt-16 flex flex-row md:flex-col justify-between min-w-full md:min-w-0 md:min-w-auto md:min-h-screen fixed '>
           <div>
-            <h3 className="uppercase font-bold">Gvozdeva Anna</h3>
-            <nav className="mt-6">
-              <NavItem to="graphic">Graphic art</NavItem>
-              <NavItem to="digital">Digital</NavItem>
-              <NavItem to="vector">Vector</NavItem>
-              <NavItem to="painting">Painting</NavItem>
+            <h3 className='uppercase font-bold'>Gvozdeva Anna</h3>
+            <nav className='mt-6'>
+              {artGroups.map((i, k) => <NavItem key={k} to={i.name}>{i.title}</NavItem>)}
             </nav>
           </div>
-          <sign className="w-28 -ml-5 -mb-7"><Image src="/sign.png" width={720} height={928}/></sign>
+          <sign className='w-28 -ml-5 -mb-7'><Image src='/sign.png' width={720} height={928} /></sign>
         </sidemenu>
-        <content className="p-10 mt-64 md:mt-0 md:pt-10 md:ml-52 pr-0 pt-0">
-          <Item link="graphic" src="/2.png" width={2221} height={2686} />
-          <Item src="/3.png" width={3508} height={2408} />
-          <Item link="digital" src="/1.png" width={2480} height={3508} />
-          <Item link="vector" src="/v1.png" width={4000} height={5607} />
-          <Item link="painting" src="/p1.png" width={3024} height={4032} />
+        <content className='p-10 mt-64 md:mt-0 md:pt-0 md:ml-52 pr-0 pt-0'>
+          {artGroups.map((i, k) =>
+            <ArtGroup key={`group-${k}`} name={i.name}>
+              {i.arts.map((art, artId) =>
+                <Art key={`group-${k}-${artId}`} {...art} />
+              )}
+            </ArtGroup>
+          )}
+          {/*<ArtGroup name={artGroups[0].name}>*/}
+          {/*  <Art link='graphic' src='/2.png' width={2221} height={2686} />*/}
+          {/*</ArtGroup>*/}
+          {/*<Art src='/3.png' width={3508} height={2408} />*/}
+          {/*<Art link='digital' src='/1.png' width={2480} height={3508} />*/}
+          {/*<Art link='vector' src='/v1.png' width={4000} height={5607} />*/}
+          {/*<Art link='painting' src='/p1.png' width={3024} height={4032} />*/}
         </content>
 
       </main>
     </>
-  )
+  );
 }
