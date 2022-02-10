80081a586764a0d42abc51ea554efd6e7877dd02
Andrew <zelenin.andrew@gmail.com>
2022-02-10 18:31:01 +0300

    added fonts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Item = (src, width, height) => <div><Image src={src} width={width} height={height} /></div>
+const NavItem = ({ children }) => <li className="list-none mt-2 text-sm">{children}</li>
 
 export default function Home() {
   return (
@@ -13,19 +14,19 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-1 flex-row min-h-screen">
-        <sidemenu className="p-10 flex flex-col justify-between min-h-screen fixed">
+        <sidemenu className="p-10 pt-16 flex flex-col justify-between min-h-screen fixed">
           <div>
-            <h3 className="uppercase">Gvozdeva Anna</h3>
-            <nav className="mt-5">
-              <li className="list-none">Digital</li>
-              <li className="list-none">Vector</li>
-              <li className="list-none">Paper</li>
-              <li className="list-none">Painting</li>
+            <h3 className="uppercase font-bold">Gvozdeva Anna</h3>
+            <nav className="mt-6">
+              <NavItem>Digital</NavItem>
+              <NavItem>Vector</NavItem>
+              <NavItem>Paper</NavItem>
+              <NavItem>Painting</NavItem>
             </nav>
             </div>
-          <sign className="w-28"><Image src="/img/sign.png" width={720} height={928}/></sign>
+          <sign className="w-28 -ml-5 -mb-7"><Image src="/img/sign.png" width={720} height={928}/></sign>
         </sidemenu>
-        <content className="p-10 ml-52"><Image src="/img/1.PNG" width={2408} height={3508} /></content>
+        <content className="p-10 ml-52 pr-0"><Image src="/img/1.PNG" width={2408} height={3508} /></content>
 
       </main>
     </div>
