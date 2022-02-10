6c60ee4b05f645317a23ffba03f8add020f5878a
Andrew <zelenin.andrew@gmail.com>
2022-02-10 18:18:26 +0300

    add first image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+const Item = (src, width, height) => <div><Image src={src} width={width} height={height} /></div>
+
 export default function Home() {
   return (
     <div>
@@ -11,19 +13,19 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-1 flex-row min-h-screen">
-        <sidemenu className="p-10 flex flex-col justify-between min-h-screen bg-gray-100 fixed">
+        <sidemenu className="p-10 flex flex-col justify-between min-h-screen fixed">
           <div>
             <h3 className="uppercase">Gvozdeva Anna</h3>
             <nav className="mt-5">
-              <li className="list-none">Paper</li>
-              <li className="list-none">Painting</li>
               <li className="list-none">Digital</li>
               <li className="list-none">Vector</li>
+              <li className="list-none">Paper</li>
+              <li className="list-none">Painting</li>
             </nav>
             </div>
           <sign className="w-28"><Image src="/img/sign.png" width={720} height={928}/></sign>
         </sidemenu>
-        <content className="p-10 ml-52">/images</content>
+        <content className="p-10 ml-52"><Image src="/img/1.PNG" width={2408} height={3508} /></content>
 
       </main>
     </div>
