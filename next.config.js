843fb7e96d7547d2a57e707bc6daf7bf39763071
Andrew <zelenin.andrew@gmail.com>
2022-03-04 11:52:22 +0300

    admin redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: process.env.ADMIN_URL,
+        permanent: true,
+      },
+    ]
+  },
 }
